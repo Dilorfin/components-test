a5476c65239d6dddb914495c4094362e3f337d45
Dilorfin <pavel.kuybida@gmail.com>
2021-10-29 13:22:08 +0300

    Make fireball self destroy after some time

--- objects/FireBall.hpp
+++ objects/FireBall.hpp
@@ -2,12 +2,20 @@
 #include "../core/GameObject.hpp"
 #include "../components/SpriteComponent.hpp"
 #include "../components/MotionComponent.hpp"
+#include "../components/TimerComponent.hpp"
 
 class FireBall final : public GameObject
 {
 public:
 	explicit FireBall(const sf::Vector2f& initPosition, const sf::Vector2f& velocity)
 	{
+		auto* timer = this->addComponent<TimerComponent>();
+		timer->setEndTime(sf::seconds(0.5).asMicroseconds());
+		timer->setTrigger([this] {
+			std::cout << "destroy" << std::endl;
+			GameObjectsManager::getInstance()->removeObject(this);
+		});
+
 		auto* sprite  = this->addComponent<SpriteComponent>("./assets/FireBall.png");
 		sprite->setSize(32, 32);
 
