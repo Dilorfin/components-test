5b464bef7439d1db16aa273d2f76bf5578099bf3
Dilorfin <pavlo_kuibida@epam.com>
2022-03-21 17:59:31 +0200

    Fix SystemLocator

--- core/SystemLocator.hpp
+++ core/SystemLocator.hpp
@@ -2,7 +2,6 @@
 #include <map>
 #include <typeinfo>
 
-#include "GameObject.hpp"
 #include "System.hpp"
 #include "GameObjectSystem.hpp"
 
@@ -27,11 +26,9 @@ public:
 
 	static SystemLocator* getInstance()
 	{
-		if (instance == nullptr)
-		{
-			instance = new SystemLocator();
-		}
-		return instance;
+		static SystemLocator instance;
+
+		return &instance;
 	}
 
 	template<typename TSystem>
@@ -41,32 +38,20 @@ public:
 		if (!getInstance()->systems.count(id))
 		{
 			auto* system = new TSystem();
-			instance->systems[id] = system;
+			getInstance()->systems[id] = system;
 			return system;
-			//return nullptr;
 		}
-		return (TSystem*)instance->systems.at(id);
+		return (TSystem*)getInstance()->systems.at(id);
 	}
-
-	/*template<typename TSystem, typename... Ts>
-	TSystem* addSystem(Ts&&... args)
-	{
-		const size_t id = typeid(TSystem).hash_code();
-		if (systems.count(id))
-		{
-			throw std::exception((std::string("System is already registered: ") + typeid(TSystem).name()).c_str());
-		}
-
-		auto* system = new TSystem(std::forward<Ts>(args)...);
-		systems[id] = system;
-		return system;
-	}*/
 
 protected:
 	friend class SceneManager;
 
 	void clear()
 	{
+		if (systems.count(0))
+			return;
+
 		const size_t id = typeid(GameObjectsManager).hash_code();
 		delete systems[id];
 		systems.erase(id);
