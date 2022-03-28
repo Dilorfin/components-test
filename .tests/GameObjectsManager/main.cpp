1689528146a86b1e932ed9c41d87afd8b0b55cd5
Dilorfin <pavlo_kuibida@epam.com>
2022-03-28 18:23:58 +0300

    Add getObjectById tests

--- .tests/GameObjectsManager/main.cpp
+++ .tests/GameObjectsManager/main.cpp
@@ -14,18 +14,47 @@ TEST_CASE("adding object")
 		auto* obj2 = new GameObject;
 		manager.add(obj1);
 		manager.add(obj2);
-		
+
 		CHECK(obj1->getId() != obj2->getId());
 	}
+
 	SUBCASE("object should be started on add")
 	{
 		GameObjectsManager manager;
 		auto* obj = new TestStartObject;
-		
+
 		CHECK_FALSE(obj->isStarted());
-		
+
 		manager.add(obj);
-		
+
 		CHECK(obj->isStarted());
+	}
+}
+
+TEST_CASE("get object by id")
+{
+	SUBCASE("object not found")
+	{
+		const GameObjectsManager manager;
+
+		const GameObject* obj = manager.getObjectById(0);
+
+		CHECK(obj == nullptr);
+	}
+
+	SUBCASE("object should be found by id")
+	{
+		GameObject* obj = new GameObject;
+
+		GameObjectsManager manager;
+		manager.add(new GameObject);
+		manager.add(obj);
+
+		const auto objId = obj->getId();
+
+		obj = manager.getObjectById(objId);
+
+		CHECK(obj != nullptr);
+		CHECK(obj->getId() == objId);
 	}
 }
