fca17458ef09362a4eff68a7d18684b9cd9f9a84
Dilorfin <pavlo_kuibida@epam.com>
2022-03-29 14:28:59 +0300

    Add SceneManager tests (#17)
    * Fix SceneManager not setting current scene id
    
    * Add tests for SceneManager

--- core/SceneManager.hpp
+++ core/SceneManager.hpp
@@ -21,7 +21,9 @@ private:
 	size_t currentSceneId = 0;
 
 	bool _switch = false;
-	size_t nextScene = 0;
+	size_t nextSceneId = 0;
+
+	SceneManager() = default;
 
 public:
 
@@ -51,7 +53,8 @@ public:
 			SystemLocator::getSystem<RenderSystem>()->setRenderTarget(render);
 		}
 
-		currentScene = scenesFabrics[nextScene]();
+		currentScene = scenesFabrics[nextSceneId]();
+		currentSceneId = nextSceneId;
 	}
 
 	[[nodiscard]] size_t getScenesCount() const
@@ -62,17 +65,33 @@ public:
 	{
 		return currentSceneId;
 	}
+	[[nodiscard]] std::string getCurrentSceneName() const
+	{
+		assert(currentScene);
+		auto predicate = [this](std::pair<std::string, size_t> pair)->bool {
+			return this->currentSceneId == pair.second;
+		};
+		const auto resultIt = std::find_if(std::begin(scenesNames), std::end(scenesNames), predicate);
+		assert(resultIt != std::end(scenesNames));
+		return resultIt->first;
+	}
 
-	void addScene(const std::string& name, const std::function<Scene* ()>& scene)
+	size_t addScene(const std::string& name, const std::function<Scene* ()>& scene)
 	{
 		assert(scenesNames.count(name) == 0);
-		scenesNames.insert({ name, scenesFabrics.size() });
+
+		const auto id = scenesFabrics.size();
+
+		scenesNames.insert({ name, id });
 		scenesFabrics.push_back(scene);
+
+		return id;
 	}
 
 	void loadScene(const std::string& name)
 	{
 		assert(scenesNames.count(name) > 0);
+
 		this->loadScene(scenesNames[name]);
 	}
 	void loadScene(const size_t& id)
@@ -81,10 +100,27 @@ public:
 		assert(id < scenesFabrics.size());
 
 		_switch = true;
-		this->nextScene = id;
+		this->nextSceneId = id;
 	}
 	void reloadScene()
 	{
 		this->loadScene(this->currentSceneId);
+	}
+
+	void clear()
+	{
+		scenesFabrics.clear();
+		scenesNames.clear();
+
+		if (currentScene)
+		{
+			delete currentScene;
+			currentScene = nullptr;
+		}
+
+		_switch = false;
+
+		currentSceneId = 0;
+		nextSceneId = 0;
 	}
 };
